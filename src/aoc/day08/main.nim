abcdac64031ca814cfaa77d7bb0f2b8f781dc29f
Samuel Laroche <slaroche@Alana.local>
2025-12-11 11:19:04 -0500

    day 08 and wip day 10

--- src/aoc/day08/main.nim
+++ src/aoc/day08/main.nim
@@ -9,67 +9,120 @@ import math
 import algorithm
 import options
 
-type Point = tuple[x, y, z: int]
-type Conn = tuple[dis: int, a, b: Point]
+type Junc = tuple[x, y, z: int]
+type Conn = tuple[dis: int, a, b: Junc]
 
-func distance(a, b: Point): int =
+
+func distance(a, b: Junc): int =
     int(sqrt(float((a.x - b.x)^2 + (a.y - b.y)^2 + (a.z - b.z)^2)))
 
-func findIndex(clusters: seq[seq[Point]]; junc: Point): Option[int] =
+
+func findIndex(clusters: seq[seq[Junc]]; junc: Junc): Option[int] =
     for index in 0..<clusters.len: 
         if junc in clusters[index]: 
             return index.some()
 
-when isMainModule:
-    # let inputs = loadExample(currentSourcePath().parentDir())
+proc partA(inputs: seq[seq[string]]): int =
     # const MaxPairs = 10
-    let inputs = loadInput(currentSourcePath().parentDir()) 
     const MaxPairs = 1000
 
+    let juncs: seq[Junc] = collect:
+        for line in inputs:
+            let values = line[0].split(',').map(
+                proc (x: string): int = x.parseInt()
+            )
+            (values[0], values[1], values[2])
+    
+    var connections: seq[Conn] = collect:
+        for i in 0..<juncs.len:
+            for j in i+1..<juncs.len:
+                (distance(juncs[i], juncs[j]), juncs[i], juncs[j])
+    
+    connections.sort(proc (a, b: Conn): int = cmp(a.dis, b.dis))
+
+    var clusters: seq[seq[Junc]] = @[]
+    for c in connections[0..<MaxPairs]:
+        var indexA = clusters.findIndex(c.a)
+        var indexB = clusters.findIndex(c.b)
+        if indexA.isNone() and indexB.isNone(): 
+            clusters.add(@[c.a, c.b])
+        elif indexA.isSome() and indexB.isNone():
+            clusters[indexA.get()].add(c.b)
+        elif indexA.isNone() and indexB.isSome():
+            clusters[indexB.get()].add(c.a)
+        elif indexA.get() == indexB.get(): 
+            continue
+        else:
+            for b in clusters[indexB.get()]:
+                clusters[indexA.get()].add(b)
+            clusters.delete(indexB.get())
+
+    clusters.sort(proc (a, b: seq[Junc]): int = cmp(b.len, a.len))
+    result = 1
+    for x in clusters[0..<3]:
+        result *= x.len
+
+proc partB(inputs: seq[seq[string]]): int = 
+    var juncs: seq[Junc] = collect:
+        for line in inputs:
+            let values = line[0].split(',').map(
+                proc (x: string): int = x.parseInt()
+            )
+            (values[0], values[1], values[2])
+    
+    var connections: seq[Conn] = collect:
+        for i in 0..<juncs.len:
+            for j in i+1..<juncs.len:
+                (distance(juncs[i], juncs[j]), juncs[i], juncs[j])
+    
+    connections.sort(proc (a, b: Conn): int = cmp(a.dis, b.dis))
+
+    var clusters: seq[seq[Junc]] = @[]
+    var index = 0
+
+    var lastConnection = none(Conn)
+    while juncs.len > 0 or clusters.len != 1:
+        let c = connections[index]
+        index += 1
+
+        var indexA = clusters.findIndex(c.a)
+        var indexB = clusters.findIndex(c.b)
+
+        if indexA.isNone() and indexB.isNone(): 
+            clusters.add(@[c.a, c.b])
+        elif indexA.isSome() and indexB.isNone():
+            clusters[indexA.get()].add(c.b)
+        elif indexA.isNone() and indexB.isSome():
+            clusters[indexB.get()].add(c.a)
+        elif indexA.get() == indexB.get(): 
+            continue
+        else:
+            for b in clusters[indexB.get()]:
+                clusters[indexA.get()].add(b)
+            clusters.delete(indexB.get())
+
+        juncs = juncs.filterIt(it != c.a)
+        juncs = juncs.filterIt(it != c.b)
+
+        lastConnection = some(c)
+    if lastConnection.isSome():
+        echo fmt"lastConnection: {lastConnection.get()}"
+        echo fmt"clusters: {clusters.len}"
+        echo fmt"juncs: {juncs.len}"
+
+        result = lastConnection.get.a.x * lastConnection.get.b.x
+    else:
+        echo fmt"no last connection"
+
+
+when isMainModule:
+    # let inputs = loadExample(currentSourcePath().parentDir())
+    let inputs = loadInput(currentSourcePath().parentDir()) 
+
     timeIt "puzzle 1":
-        let junc: seq[Point] = collect:
-            for line in inputs:
-                let values = line[0].split(',').map(
-                    proc (x: string): int = x.parseInt()
-                )
-                (values[0], values[1], values[2])
-        
-        var connections: seq[Conn] = collect:
-            for i in 0..<junc.len:
-                for j in i+1..<junc.len:
-                    (distance(junc[i], junc[j]), junc[i], junc[j])
-        
-        connections.sort(proc (a, b: Conn): int = cmp(a.dis, b.dis))
-
-        var clusters: seq[seq[Point]] = @[]
-        for c in connections[0..<MaxPairs]:
-            var indexA = clusters.findIndex(c.a)
-            var indexB = clusters.findIndex(c.b)
-            if indexA.isNone() and indexB.isNone(): 
-                clusters.add(@[c.a, c.b])
-            elif indexA.isSome() and indexB.isNone():
-                clusters[indexA.get()].add(c.b)
-            elif indexA.isNone() and indexB.isSome():
-                clusters[indexB.get()].add(c.a)
-            elif indexA.get() == indexB.get(): 
-                continue
-            else:
-                for b in clusters[indexB.get()]:
-                    clusters[indexA.get()].add(b)
-                clusters.delete(indexB.get())
-
-        clusters.sort(proc (a, b: seq[Point]): int = cmp(b.len, a.len))
-        # for i in 0..<3:
-        #     echo fmt"cluster: {clusters[i].len}"
-        var total = 1
-        for x in clusters[0..<3]:
-            total *= x.len
-        echo fmt"clusters: {total}"
-
+        echo fmt"solution: {partA(inputs)}"
 
     echo ""
 
-
     timeIt "puzzle 2":
-        total = 0
-        echo fmt"solution: {total}"
+        echo fmt"solution: {partB(inputs)}"
--- src/aoc/day10/main.nim
+++ src/aoc/day10/main.nim
@@ -15,7 +15,8 @@ type Machine = ref object of RootObj
     buttons: seq[seq[int]]
     joltage: string
 
-func newMachine(lights: string, buttons: seq[string], joltage: string): Machine =
+func newMachine(lights: string, buttons: seq[string],
+        joltage: string): Machine =
     result = new Machine
     result.buttons = collect:
         for button in buttons:
@@ -27,10 +28,10 @@ func newMachine(lights: string, buttons: seq[string], joltage: string): Machine 
 
 type Button = seq[int]
 
-proc distance(a: string): int = 
-    return a.strip(chars={'.'}).len
+proc distance(a: string): int =
+    return a.strip(chars = {'.'}).len
 
-proc flip(light: char): char = 
+proc flip(light: char): char =
     if light == '#': return '.'
     if light == '.': return '#'
 
@@ -40,12 +41,12 @@ func first[T](l: seq[T]): Option[T] =
         return some(item)
     return none(T)
 
-proc press(lights: string, buttons: Button): string = 
+proc press(lights: string, buttons: Button): string =
     result = lights
     for button in buttons:
         result[button] = lights[button].flip
 
-proc partA(inputs: seq[seq[string]]): int = 
+proc partA(inputs: seq[seq[string]]): int =
     let machines = collect:
         for input in inputs:
             newMachine(input[0][1..^2], input[1..^2], input[^1])
@@ -60,23 +61,24 @@ proc partA(inputs: seq[seq[string]]): int =
             if c > 10: 
                 break
 
-            var lastButton = machine.buttons.filter(proc (x: Button): bool = x notin buttonsPressed).first()
-            if lastButton.isNone(): 
+            var lastButton = machine.buttons.filter(proc (
+                    x: Button): bool = x notin buttonsPressed).first()
+            if lastButton.isNone():
                 break
 
             let og = q
             var current = q
             for button in machine.buttons:
-                if button in buttonsPressed: 
+                if button in buttonsPressed:
                     continue
 
                 if current.distance > q.press(button).distance:
                     current = q.press(button)
                     lastButton = some(button)
 
-            buttonsPressed.add(lastButton.get)
-            q = q.press(lastButton.get)
-            echo fmt"og: {og}, q: {q}, lastButton: {lastButton.get}"
+            buttonsPressed.add(lastButton.get())
+            q = q.press(lastButton.get())
+            echo fmt"og: {og}, q: {q}, lastButton: {lastButton.get()}"
         echo fmt"buttonsPressed: {buttonsPressed.len}"
         quit 0
 
@@ -90,7 +92,7 @@ when isMainModule:
     let inputs = loadExample(currentSourcePath().parentDir())
     # let inputs = loadInput(currentSourcePath().parentDir())
 
-    
+
 
     timeIt "puzzle 1":
         echo fmt"solution: {partA(inputs)}"
